4058ca24a81f3b993c5c9231678d22c14c266805
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-12-08 06:40:10 +0700

    membuat username menjadi uniq

--- model/user.go
+++ model/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID         uint      `json:"id" gorm:"primaryKey"`
-	Username   string    `json:"username"`
+	Username   string    `json:"username" gorm:"unique;not null"`
 	Password   string    `json:"password"`
 	CreatedAt  time.Time `json:"created_at"`
 	CreatedBy  string    `json:"created_by"`
